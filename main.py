86f55d2ea927f73f1bd170b388eecd6bdd4caecd
Michael Choi <132119138+choimikey@users.noreply.github.com>
2023-07-25 15:54:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import json
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response.status_code)
+
+# You can write if statements like this if you want for error code
 # if response.status_code != 200:
 #     raise Exception("Bad response from ISS API")
-
 # if response.status_code == 404:
 #     raise Exception("That resource does not exist.")
 # elif response.status_code == 401:
 #     raise Exception("You are not authorised to access this data.")
-
-# Instead of raising an exception for every status code possible you can do something like this:
+# But instead of raising an exception for every status code possible you can do something like this:
 response.raise_for_status()
 
 data = response.json()
